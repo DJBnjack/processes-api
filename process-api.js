7f34b3f4059cb86ffe8d02475d72cad18aca632d
Johannes Bertens <johannes.bertens@live.nl>
2015-11-19 23:10:24 +0100

    Love the simplicity

--- process-api.js
+++ process-api.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var processes = require('./api/processes.js');
+
 var app = express();
 
 // Setup process api
@@ -10,11 +11,7 @@ app.use('/', router);
 // Disable cache
 app.disable('etag');
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+// Fire it up
+app.listen(3000);
 
 
