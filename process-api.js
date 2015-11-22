31b1384d31601c56af2abd4bdbaaa2ee416a2ee6
Johannes Bertens <johannes.bertens@live.nl>
2015-11-22 15:56:52 +0100

    Added cross-origin headers again...

--- process-api.js
+++ process-api.js
@@ -9,6 +9,19 @@ setInterval(function(){
 }, 60000);
 
 var app = express();
+ 
+// Add cross-origin headers
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // Setup process api
 var router = express.Router();
